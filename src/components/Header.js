5bcbb4e49169f9111380d6d58d77569271940715
nhung1291 <hongnhungnguyen1291@gmail.com>
2023-03-02 00:09:24 -0500

    feat/header-button-components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import Button from './Button'
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Hello'/>
+            <Button backgroundColor='red' text='Hi' onClick={onClick}/>
         </header>
     )
 }
