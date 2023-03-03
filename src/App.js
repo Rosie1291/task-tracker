a62b6afcccf1bd5708cbd877e0df6ad3a5309b08
nhung1291 <hongnhungnguyen1291@gmail.com>
2023-03-03 12:58:53 -0500

    feat/useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "School Meeting",
+      day: "Feb 6th at 10:00pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Grocery Shopping",
+      day: "Feb 8th at 5:30pm",
+      reminder: false,
+    },
+  ])
+
+  const deleteTask = (id) => (
+    console.log('delete', id)
+  )
+
   return (
     <>
       <div className="container">
         <Header />
-        <Tasks />
+        <Tasks tasks={tasks} onDelete={deleteTask} />
       </div>
 
     </>
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,31 +1,12 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "Feb 5th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "School Meeting",
-    day: "Feb 6th at 10:a0pm",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "Grocery Shopping",
-    day: "Feb 8th at 5:30pm",
-    reminder: false,
-  },
-];
+import Task from './Task'
 
-const Tasks = () => {
-  return (
+const Tasks = ({ tasks, onDelete }) => {
+    return (
     // empty fragment
     <>
       {/* map takes in a function: for each task, return a jsx */}
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3> // each parent should have a key prop
+        <Task key={task.id} task={task} onDelete={onDelete}/> // each parent should have a key prop
       ))}
     </>
   );
